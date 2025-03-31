3a2b53d4c93e6f85727f761958e872d233fd4d0f
Joseph Herrera <josephherreraleon@gmail.com>
2025-03-31 12:20:34 -0600

    Add state to component

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,10 +1,34 @@
-import Profile from "./Profile";
+import { sculptureList } from "../data/data";
+import { useState } from "react";
 
 export default function Gallery() {
+  const [index, setIndex] = useState(0);
+  const [showMore, setShowMore] = useState(false);
+
+  function handleClick() {
+    setIndex(index + 1);
+  }
+
+  function handleMoreClick() {
+    setShowMore(!showMore);
+  }
+
+  let sculpture = sculptureList[index];
+
   return (
-    <section>
-      <h1>Amazing scientists</h1>
-      <Profile />
-    </section>
+    <>
+      <h2>
+        <i>{sculpture.name}</i> by {sculpture.artist}
+      </h2>
+      <button onClick={handleClick}>Next</button>
+      <h3>
+        ({index + 1} of {sculptureList.length})
+      </h3>
+      <button onClick={handleMoreClick}>
+        {showMore ? "Hide" : "Show"} details
+      </button>
+      {showMore && <p>{sculpture.description}</p>}
+      <img src={sculpture.url} alt={sculpture.alt} />
+    </>
   );
 }
